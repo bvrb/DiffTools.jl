75be99bc0be14b97260a485edf1fdad2c39766e0
bvrb <bvrb@posteo.org>
2021-06-27 21:22:42 +0200

    Started splitting code into separate files; added test cases; added workaround for unexpected Float16 conversion

--- src/DiffTools.jl
+++ src/DiffTools.jl
@@ -1,15 +1,23 @@
 module DiffTools
 
-using ForwardDiff, StaticArrays
+#using ForwardDiff, StaticArrays
+using ForwardDiff: ForwardDiff, Dual
+using StaticArrays 
+
+# Modified from https://github.com/JuliaDiff/ForwardDiff.jl/blob/909976d719fdbd5fec91a159c6e2d808c45a770f/src/dual.jl#L374 
+# in order to work around https://github.com/JuliaDiff/ForwardDiff.jl/issues/362 , https://github.com/JuliaDiff/ForwardDiff.jl/issues/492 , https://github.com/JuliaDiff/ForwardDiff.jl/pull/419
+# which will otherwise lead to loss of precision in some cases when using Integer literals
+Base.float(d::Dual{T,V,N}) where {T,V,N} = convert(Dual{T,promote_type(V, Float64),N}, d)
+Base.AbstractFloat(d::Dual{T,V,N}) where {T,V,N} = convert(Dual{T,promote_type(V, Float64),N}, d)
 
 ∂(f, x) = ForwardDiff.derivative(f, x)
 
 
-function vector_to_complex(a::SVector{2}) #TODO type, number of dimensions
+function vector_to_complex(a::SVector{2})
     return complex(a[1], a[2])
 end
 
-function complex_to_vector(z::Complex) #TODO type, number of dimensions
+function complex_to_vector(z::Complex)
     return SVector{2}(real(z), imag(z))
 end
 
---
+++ test/test_derivative.jl
@@ -0,0 +1,25 @@
+using DiffTools: ∂
+using Test
+
+@testset "Example derivatives" begin
+    @testset begin 
+        f(z) = 3z*z - 4z + 10 
+        @test ∂(f, 2) == 8
+        @test ∂(f, 2 + 0im) == 8 + 0im
+        @test ∂(f, 1 - 2im) == 2 - 12im
+    end
+
+    @testset begin
+        f(z) = 2z*z*z - z + im * (12z + 4)
+        @test ∂(f, 2 + 0im) == 23 + 12im
+        @test ∂(f, im) == -7 + 12im
+        @test ∂(f, -1 - im) == -1 + 24im
+    end
+
+    @testset begin 
+        f(z) = exp(3*z) 
+        @test ∂(f, 0 + 0im) == 3 + 0im
+        @test ∂(f, 1 + 0im) == 3ℯ^3 + 0im 
+        @test ∂(f, -1 + im) == 3ℯ^(-3) * cis(3)
+    end
+end
