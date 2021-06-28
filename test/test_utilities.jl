23b7c1a214d293bd4813045ebe0ba25483e1f051
bvrb <bvrb@posteo.org>
2021-06-28 23:58:06 +0200

    Refactored derivative code; Added second complex derivative; Added more tests

--- test/test_utilities.jl
+++ test/test_utilities.jl
@@ -13,4 +13,11 @@ end
     @test DiffTools.complex_to_vector(-0.2 - 20.9im) == SVector{2}(-0.2, -20.9)
 end
 
+@testset "Extract first column" begin
+    A = @SMatrix [1 3; 2 4]
+    @test DiffTools.first_column(A) == SVector{2}(1, 2)
+    B = @SMatrix [1.5 3.5; 2.5 4.5]
+    @test DiffTools.first_column(B) == SVector{2}(1.5, 2.5)
+end
+
 ;
